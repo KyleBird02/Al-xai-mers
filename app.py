3f50d6d35348411b07389e5310604661c0ca9914
Kyle <81643823+KyleBird02@users.noreply.github.com>
2023-10-29 16:35:15 +0530

    Scuffed LIME XAI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import os
 import numpy as np
 from PIL import Image
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from keras.models import load_model
 import base64
 from io import BytesIO
+from lime import lime_image
+from skimage.segmentation import mark_boundaries
 
 app = Flask(__name__)
 
@@ -19,27 +21,60 @@ num_classes = 4
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle the uploaded image
-        uploaded_file = request.files['file']
-        if uploaded_file.filename != '':
-            img = Image.open(uploaded_file).convert('RGB')
-            img_resized = img.resize(input_shape)
-            img_array = np.array(img_resized, dtype='float32')
-            img_array = np.expand_dims(img_array, axis=0)
-            img_array /= 255.0
+        if 'ml_predict' in request.form:
+            uploaded_file = request.files['file']
+            if uploaded_file.filename != '':
+                img = Image.open(uploaded_file).convert('RGB')
+                img_resized = img.resize(input_shape)
+                img_array = np.array(img_resized, dtype='float32')
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array /= 255.0
 
-            # Make predictions
-            predictions = model.predict(img_array)
-            predicted_class_index = np.argmax(predictions[0])
-            cats = ['Mild Dementia', 'Moderate Dementia', 'Very mild Dementia', 'Non Demented']
-            predicted_class = cats[predicted_class_index]
+                # Make predictions
+                predictions = model.predict(img_array)
+                predicted_class_index = np.argmax(predictions[0])
+                cats = ['Mild Dementia', 'Moderate Dementia', 'Very mild Dementia', 'Non Demented']
+                predicted_class = cats[predicted_class_index]
 
-            # Encode the image as a Base64 string
-            buffered = BytesIO()
-            img.save(buffered, format="JPEG")
-            img_base64 = base64.b64encode(buffered.getvalue()).decode()
+                # Encode the original image as a Base64 string
+                buffered = BytesIO()
+                img.save(buffered, format="JPEG")
+                img_base64 = base64.b64encode(buffered.getvalue()).decode()
 
-            return render_template('result.html', image=img_base64, predicted_class=predicted_class)
+                return render_template('result.html', image=img_base64, predicted_class=predicted_class)
+
+        if 'lime_predict' in request.form:
+            uploaded_file = request.files['file']
+            if uploaded_file.filename != '':
+                img = Image.open(uploaded_file).convert('RGB')
+                img_resized = img.resize(input_shape)
+                img_array = np.array(img_resized, dtype='float32')
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array /= 255.0
+
+                # Make predictions
+                predictions = model.predict(img_array)
+                predicted_class_index = np.argmax(predictions[0])
+                cats = ['Mild Dementia', 'Moderate Dementia', 'Very mild Dementia', 'Non Demented']
+                predicted_class = cats[predicted_class_index]
+
+                # Lime explanation
+                explainer = lime_image.LimeImageExplainer()
+                explanation = explainer.explain_instance(img_array[0], model.predict, top_labels=1, num_samples=50)
+                temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=True, num_features=4, hide_rest=False)
+                lime_img = BytesIO()
+                plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+                plt.title("Lime Explanation")
+                plt.axis('off')
+                plt.savefig(lime_img, format='png')
+                lime_img = base64.b64encode(lime_img.getvalue()).decode()
+
+                # Encode the original image as a Base64 string
+                buffered = BytesIO()
+                img.save(buffered, format="JPEG")
+                img_base64 = base64.b64encode(buffered.getvalue()).decode()
+
+                return render_template('result.html', image=img_base64, lime_image=lime_img, predicted_class=predicted_class)
 
     return render_template('index.html')
 
